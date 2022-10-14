7aa1e410b4f3bd0fbac0574510473f077c2ff66f
h-yung <helen.yung.h@gmail.com>
2022-10-14 14:56:31 -0400

    updated api url

--- frontend/src/api/twitter.js
+++ frontend/src/api/twitter.js
@@ -1,6 +1,8 @@
 //fetch latest tweets, taking in username input
+const API_URL = "https://tweet-sentiment-analyzer.up.railway.app";
+
 export const getTweets = async (username) => {
-  const response = await fetch(`http://localhost:3001/user/${username}`);
+  const response = await fetch(`${API_URL}/user/${username}`);
   const data = await response.json();
   return data;
 };
